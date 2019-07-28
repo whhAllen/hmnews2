3f9751bf04c1070f191f498348583f2e617d3608
whhallen <546432714@qq.com>
2019-07-28 17:20:36 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+     <!-- <h1>App----</h1> -->
+    <!-- <van-button>默认按钮</van-button> -->
+<!-- <van-swipe :autoplay="3000" indicator-color="white">
+  <van-swipe-item>1</van-swipe-item>
+  <van-swipe-item>2</van-swipe-item>
+  <van-swipe-item>3</van-swipe-item>
+  <van-swipe-item>4</van-swipe-item>
+</van-swipe> -->
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+  },
+
+}
+</script>
+
+<style lang="less">
+
+</style>
