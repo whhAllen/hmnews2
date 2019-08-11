511f5d6e92de2d4f0eda45072b2a5348e3f3ef9e
whhallen <546432714@qq.com>
2019-08-11 10:29:41 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 import router from '@/router'
-
+import 'amfe-flexible/index.min.js'
 Vue.use(Vant)
 Vue.config.productionTip = false
 
